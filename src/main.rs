da8c971a8a09946d28ab7c105df6481147c35e02
Naveen-Lab <naveenbraphael@gmail.com>
2024-06-04 10:02:36 +0530

    My modifications to make it more rustic

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,122 @@
+use std::ops::{AddAssign, Index, IndexMut};
+
+#[derive(Clone, Copy)]
+enum CellState {
+    Queen,
+    QueenSeen(u8),
+}
+
+impl CellState {
+    fn queen_candidate(&self) -> bool {
+        matches!(self, CellState::QueenSeen(0))
+    }
+}
+impl Default for CellState {
+    fn default() -> Self {
+        CellState::QueenSeen(0)
+    }
+}
+impl AddAssign<i8> for CellState {
+    fn add_assign(&mut self, rhs: i8) {
+        match (self, rhs > 0) {
+            (CellState::Queen, _) => unreachable!(),
+            (CellState::QueenSeen(i), true) => *i += rhs as u8,
+            (CellState::QueenSeen(i), false) => *i -= (-rhs) as u8,
+        }
+    }
+}
+
 struct QueenBoard {
-    board: Vec<Vec<i32>>,
+    len: u8,
+    board: Vec<Vec<CellState>>,
+}
+
+impl Index<(u8, u8)> for QueenBoard {
+    type Output = CellState;
+
+    fn index(&self, index: (u8, u8)) -> &Self::Output {
+        &self.board[index.0 as usize][index.1 as usize]
+    }
+}
+
+impl IndexMut<(u8, u8)> for QueenBoard {
+    fn index_mut(&mut self, index: (u8, u8)) -> &mut Self::Output {
+        &mut self.board[index.0 as usize][index.1 as usize]
+    }
 }
 
 impl QueenBoard {
-    fn new(size: usize) -> Self {
+    fn new(size: u8) -> Self {
         // Initialize the board with 0s
         QueenBoard {
-            board: vec![vec![0; size]; size],
+            len: size as u8,
+            board: vec![vec![CellState::default(); size as usize]; size as usize],
         }
     }
 
     fn print_board(&self) {
         for row in &self.board {
             for &cell in row {
-                if cell == -1 {
-                    print!("Q ");
-                } else {
-                    print!("_ ");
-                }
+                match cell {
+                    CellState::Queen => print!("Q "),
+                    CellState::QueenSeen(_) => print!("_ "),
+                };
             }
             println!(); // Newline after each row
         }
     }
-
-    fn add_queen(&mut self, r: usize, c: usize) -> bool {
-        if self.board[r][c] > 0 {
-            return false;
-        }
-
-        self.modify_board(r, c, 1);
-        true
+    fn can_place_queen(&self, r: u8, c: u8) -> bool {
+        self[(r, c)].queen_candidate()
     }
 
-    fn remove_queen(&mut self, r: usize, c: usize) {
-        if self.board[r][c] == 1 {
-            return;
-        }
-
-        self.modify_board(r, c, -1);
+    fn add_queen(&mut self, r: u8, c: u8) {
+        self[(r, c)] = CellState::Queen;
+        self.modify_board_other_cells(r, c, true);
     }
 
-    fn modify_board(&mut self, r: usize, c: usize, inc: i32) {
-        self.board[r][c] += -inc;
+    fn remove_queen(&mut self, r: u8, c: u8) {
+        self[(r, c)] = CellState::default();
+        self.modify_board_other_cells(r, c, false);
+    }
 
-        let mut k = 1;
-        while r + k < self.board.len() {
-            self.board[r + k][c] += inc;
+    fn modify_board_other_cells(&mut self, r: u8, c: u8, to_increase: bool) {
+        let inc = if to_increase { 1 } else { -1 };
+        for k in 1..8 {
+            if r + k < self.len {
+                self[(r + k, c)] += inc;
+            }
+            if r + k < self.len && c + k < self.len {
+                self[(r + k, c + k)] += inc;
+            }
+            if c + k < self.len {
+                self[(r, c + k)] += inc;
+            }
+            if r >= k && c + k < self.len {
+                self[(r - k, c + k)] += inc;
+            }
+            if r >= k {
+                self[(r - k, c)] += inc;
+            }
+            if r >= k && c >= k {
+                self[(r - k, c - k)] += inc;
+            }
             if c >= k {
-                self.board[r + k][c - k] += inc;
+                self[(r, c - k)] += inc;
             }
-            if c + k < self.board[0].len() {
-                self.board[r + k][c + k] += inc;
+            if c >= k && r + k < self.len {
+                self[(r + k, c - k)] += inc;
             }
-            k += 1;
         }
     }
 
-    fn solve(&mut self, r: usize) -> bool {
-        if r == self.board.len() {
+    fn solve(&mut self, r: u8) -> bool {
+        if r == self.len {
             return true;
         }
 
-        for c in 0..self.board[0].len() {
-            if self.add_queen(r, c) {
+        for c in 0..self.len {
+            if self.can_place_queen(r, c) {
+                self.add_queen(r, c);
                 if self.solve(r + 1) {
                     return true;
                 }
